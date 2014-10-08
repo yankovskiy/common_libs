30e719e3fd4ff21df74218c7ae1d1927f36434d1
Artem Yankovskiy <artemyankovskiy@gmail.com>
2014-10-08 20:58:55 +1100

    Updated libs

--- abslib/src/ru/neverdark/abs/OnCallback.java
+++ abslib/src/ru/neverdark/abs/OnCallback.java
@@ -16,10 +16,6 @@
 
 package ru.neverdark.abs;
 
-import android.os.Bundle;
-
-import com.actionbarsherlock.app.SherlockFragmentActivity;
-
-public abstract class UfoFragmentActivity extends SherlockFragmentActivity implements CommonApi {
+public interface OnCallback {
 
 }
--- abslib/src/ru/neverdark/abs/UfoDialogFragment.java
+++ abslib/src/ru/neverdark/abs/UfoDialogFragment.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (C) 2014 Artem Yankovskiy (artemyankovskiy@gmail.com).
+ *      This program is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *  
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *  
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package ru.neverdark.abs;
 
 import android.app.AlertDialog;
@@ -8,17 +23,11 @@ import android.view.View;
 
 import com.actionbarsherlock.app.SherlockDialogFragment;
 
-interface PrivateInterface {
-    
-}
-
-
-
 public abstract class UfoDialogFragment extends SherlockDialogFragment implements CommonApi {
-    protected AlertDialog.Builder mAlertDialog;
-    protected View mView;
-    protected PrivateInterface mCallback;
-    protected Context mContext;
+    private AlertDialog.Builder mAlertDialog;
+    private View mView;
+    private OnCallback mCallback;
+    private Context mContext;
     
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -34,8 +43,31 @@ public abstract class UfoDialogFragment extends SherlockDialogFragment implement
         mAlertDialog.setView(mView);
     }
     
-    protected void setCallback(PrivateInterface callback) {
+    public void setCallback(OnCallback callback) {
         mCallback = callback;
     }
     
+    protected OnCallback getCallback() {
+        return mCallback;
+    }
+    
+    protected View getDialogView() {
+        return mView;
+    }
+    
+    protected Context getContext() {
+        return mContext;
+    }
+    
+    protected AlertDialog.Builder getAlertDialog() {
+        return mAlertDialog;
+    }
+    
+    protected void setContext(Context context) {
+        mContext = context;
+    }
+    
+    protected void setDialogView(View view) {
+        mView = view;
+    }
 }
